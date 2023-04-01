805faa1588ffedb37c414b85940715510d7fc62b
zullapo <pcluz62@gmail.com>
2023-04-01 20:04:04 -0300

    Criação de tema da aplicação
    - Cor primária e secundária
    - Fontes

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -24,22 +24,21 @@ class TransactionList extends StatelessWidget {
                       const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                   decoration: BoxDecoration(
                       border: Border.all(
-                    color: Colors.purple,
+                    color: Theme.of(context).colorScheme.primary,
                     width: 2,
                   )),
                   padding: const EdgeInsets.all(10),
                   child: Text(
                     "R\$${t.value?.toStringAsFixed(2)}",
-                    style: const TextStyle(
+                    style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
-                        color: Colors.purple),
+                        color: Theme.of(context).colorScheme.primary),
                   )),
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                 Text(
                   t.title!,
-                  style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
                 Text(DateFormat('d MMM y').format(t.date!),
                     style: const TextStyle(color: Colors.grey))
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,25 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage());
+    final ThemeData theme = ThemeData();
+    return MaterialApp(
+        home: const MyHomePage(),
+        theme: theme.copyWith(
+            colorScheme: theme.colorScheme.copyWith(
+              primary: Colors.purple,
+              secondary: Colors.amber,
+            ),
+            textTheme: theme.textTheme.copyWith(
+                titleLarge: const TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black)),
+            appBarTheme: const AppBarTheme(
+                titleTextStyle: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold))));
   }
 }
 
