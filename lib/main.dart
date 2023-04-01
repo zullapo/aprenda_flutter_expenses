5392eefd882006955c7e20dd5ef49acb9dd73f18
zullapo <pcluz62@gmail.com>
2023-03-31 21:10:39 -0300

    Adicionar text fields e text button para criação do formulário de transactions

--- lib/main.dart
+++ lib/main.dart
@@ -31,50 +31,65 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Despesas Pessoais")),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(
-                  child: Card(
-                      elevation: 5,
-                      color: Colors.blue,
-                      child: Text("Gráfico"))),
-              Column(
-                  children: _transactions
-                      .map((Transaction t) => Card(
-                              child: Row(children: [
-                            Container(
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 15, vertical: 10),
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                  color: Colors.purple,
-                                  width: 2,
-                                )),
-                                padding: const EdgeInsets.all(10),
-                                child: Text(
-                                  "R\$${t.value?.toStringAsFixed(2)}",
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20,
-                                      color: Colors.purple),
-                                )),
-                            Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    t.title!,
-                                    style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(DateFormat('d MMM y').format(t.date!),
-                                      style:
-                                          const TextStyle(color: Colors.grey))
-                                ])
-                          ])))
-                      .toList())
-            ]));
+        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          const SizedBox(
+              child: Card(
+                  elevation: 5, color: Colors.blue, child: Text("Gráfico"))),
+          Column(
+              children: _transactions
+                  .map((Transaction t) => Card(
+                          child: Row(children: [
+                        Container(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                              color: Colors.purple,
+                              width: 2,
+                            )),
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              "R\$${t.value?.toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.purple),
+                            )),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                t.title!,
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Text(DateFormat('d MMM y').format(t.date!),
+                                  style: const TextStyle(color: Colors.grey))
+                            ])
+                      ])))
+                  .toList()),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(children: [
+                TextFormField(
+                    decoration: const InputDecoration(labelText: "Título")),
+                TextFormField(
+                    decoration:
+                        const InputDecoration(labelText: "Valor (R\$)")),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                        onPressed: () {},
+                        child: const Text("Nova transação",
+                            style: TextStyle(color: Colors.purple)))
+                  ],
+                )
+              ]),
+            ),
+          )
+        ]));
   }
 }
