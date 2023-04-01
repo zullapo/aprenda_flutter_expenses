c12239cb1d27d58f973db55600074ed2c944e92a
zullapo <pcluz62@gmail.com>
2023-04-01 17:54:58 -0300

    Usando ListView para lista de transactions

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -12,42 +12,40 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-            children: transactions
-                .map((Transaction t) => Card(
-                        child: Row(children: [
-                      Container(
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 15, vertical: 10),
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          )),
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                            "R\$${t.value?.toStringAsFixed(2)}",
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.purple),
-                          )),
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              t.title!,
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                            Text(DateFormat('d MMM y').format(t.date!),
-                                style: const TextStyle(color: Colors.grey))
-                          ])
-                    ])))
-                .toList()),
-      ),
-    );
+        height: 300,
+        child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (context, index) {
+            Transaction t = transactions[index];
+            return Card(
+                child: Row(children: [
+              Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  )),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    "R\$${t.value?.toStringAsFixed(2)}",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  )),
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Text(
+                  t.title!,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(DateFormat('d MMM y').format(t.date!),
+                    style: const TextStyle(color: Colors.grey))
+              ])
+            ]));
+          },
+        ));
   }
 }
