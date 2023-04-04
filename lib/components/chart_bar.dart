9b31e19a7f18302d62009d94f0a927b3cbe97fe5
zullapo <pcluz62@gmail.com>
2023-04-03 22:38:32 -0300

    Adicionado SizedBox com height 20 no Text do transaction value
    Quando o value da transaction era maior, o FractionallySizedBox (barra de porcentagem) ficava menor

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -15,27 +15,42 @@ class ChartBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      FittedBox(child: Text(value.toStringAsFixed(2))),
+      SizedBox(
+        height: 20,
+        child: FittedBox(
+          child: Text(
+            value.toStringAsFixed(2),
+          ),
+        ),
+      ),
       const SizedBox(height: 5),
       SizedBox(
-          height: 60,
-          width: 10,
-          child: Stack(alignment: Alignment.bottomCenter, children: [
+        height: 60,
+        width: 10,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
             Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey, width: 1.0),
-                    color: const Color.fromRGBO(220, 220, 220, 1),
-                    borderRadius: BorderRadius.circular(5))),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                color: const Color.fromRGBO(220, 220, 220, 1),
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
             FractionallySizedBox(
-                heightFactor: percentage,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(5)),
-                ))
-          ])),
+              heightFactor: percentage,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       const SizedBox(height: 5),
-      Text(label)
+      Text(label),
     ]);
   }
 }
